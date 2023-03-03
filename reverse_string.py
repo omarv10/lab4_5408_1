584d6deb9a4cafeb2ffb72dbf7402ef7369972be
Jason Desantiago <72270805+Sintect@users.noreply.github.com>
2023-03-02 20:12:58 -0800

    removing

--- reverse_string.py
+++
@@ -1,9 +0,0 @@
-# names: omar, elijah & jason 
-#group 9
-#cse 5408 Spring 2023 Lab 4
-
-def reversed_input(user_string):
-    return user_string[::-1]
-
-txt = input("Input a string: ")
-print(reversed_input(txt))
